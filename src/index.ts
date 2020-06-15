f563e705d0bec88122870a11be5b68f711455ec9
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-15 18:51:51 +0300

    implements characters sort

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-
+const charactersCollection = new CharactersCollection(`huErcFlsAkklGTPovnbxZWQ`);
 const numberCollection = new NumberCollection([10, 2, -5, 0]);
-const sorter = new Sorter(numberCollection);
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(sorter.collection);
