16ede465db675cf5af5b529081d7fbdca4f6e293
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-15 22:19:07 +0300

    implements LinkedList

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
+import { LinkedList } from './LinkedList';
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 
-const charactersCollection = new CharactersCollection(`huErcFlsAkklGTPovnbxZWQ`);
-const numberCollection = new NumberCollection([10, 2, -5, 0]);
-const sorter = new Sorter(charactersCollection);
+
+
+// const numberCollection = new NumberCollection([10, 2, -5, 0]);
+// const sorter = new Sorter(numberCollection);
+// sorter.sort();
+// console.log(numberCollection.data);
+
+// const charactersCollection = new CharactersCollection(`huErcFlsAkklGTPovnbxZWQ`);
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(5);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(sorter.collection);
+linkedList.print();
