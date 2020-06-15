accde783b1ffb5efaa6fcbe70ef5391522890422
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-15 16:29:12 +0300

    create class Sorter

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log(`hi there!`);
+class Sorter {
+
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+
+  }
+}
+
+const sorter = new Sorter([10, 2, -5, 0])
+sorter.sort();
+console.log(sorter.collection);
